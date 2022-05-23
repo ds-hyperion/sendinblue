71adeb800dd3b8b0f38c5def5c94100a17fce304
d4rk0s <gregory@d4rk0s.net>
2022-05-23 12:11:01 +0200

    bugfix

--- hyperion-sendinblue.php
+++ hyperion-sendinblue.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Adopte un corail / recif = Emailing =
+ * Plugin Name: Hyperion - Module Sendinblue
  * Plugin URI:
- * Description: Gestion des emails
+ * Description: Gestion des emails via sendinblue
  * Version: 0.1
  * Requires PHP: 8.1
  * Author: Benoit DELBOE & Grégory COLLIN
--- src/Service/AbstractSendinblueService.php
+++ src/Service/AbstractSendinblueService.php
@@ -2,13 +2,15 @@
 
 namespace Hyperion\Sendinblue\Service;
 
+use Hyperion\Sendinblue\Plugin;
+
 abstract class AbstractSendinblueService
 {
     protected $apiInstance;
 
     public function __construct()
     {
-        $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', '');
+        $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', get_option(Plugin::SENDINBLUE_API_KEY_OPTION));
 
         $this->apiInstance = new SendinBlue\Client\Api\ContactsApi(
             new GuzzleHttp\Client(),
